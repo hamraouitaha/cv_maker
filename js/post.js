9a799743a4a5de86348a77d72185b4930dd15afc
taha <taha.toutou97@gmail.com>
2019-01-31 17:11:31 +0100

    type de photo

---
+++ js/post.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function(){
+         jQuery(".td").click(function(t) {
+			 idc=jQuery(this).attr("id");
+			 
+			 type=jQuery(this).attr("data-type");
+			 ch='<input type="'+type+'" class="tst" />';
+			 if(idc == "T6")
+			ch='<textarea cols="110" rows="4" type="text" class="tst"></textarea>';
+			 if(idc=="img")
+			 {
+				// jQuery("#img").replaceWith('<input type="file" name="myfile" id="imgv">');
+				alert("oke");
+				
+			 }
+			
+			c=jQuery(this).text();
+			jQuery(this).replaceWith(ch);
+			jQuery(".tst").val(c);
+		jQuery(".tst").blur(function(t) {
+		k=jQuery(this).val();		
+		jQuery(this).replaceWith('<span class="td" >' + k + '</span>');		
+		jQuery.post(
+            "index.php", 
+            {
+                username : jQuery(this).val(),
+				id : idc,
+            },
+           function(data){
+                jQuery(".tst").html(data);
+           });
+		   window.location.reload(".td");
+		}) ;
+		 });	
+		});
